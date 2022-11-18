37f45dcc2e36cea985970c829233e6b75e0f2174
João Prado <joaopedro0128@hotmail.com>
2022-11-18 17:59:07 -0300

    feat: configures redis as cache store
    Signed-off-by: João Prado <joaopedro0128@hotmail.com>

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
-import { Module } from '@nestjs/common';
+import { CacheModule, Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeOrmConfig } from '../configs/typeorm.config';
+import { cacheConfig } from '../configs/cache.config';
 import { ConfigModule } from '@nestjs/config';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
@@ -9,6 +10,7 @@ import { AuthModule } from './auth/auth.module';
   imports: [
     ConfigModule.forRoot(),
     TypeOrmModule.forRoot(typeOrmConfig),
+    CacheModule.register(cacheConfig),
     UserModule,
     AuthModule,
   ],
