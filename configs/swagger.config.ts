25111fd5f2bcb6b3d1c34cb206e716e7ebc18a98
João Prado <joaopedro0128@hotmail.com>
2022-11-23 21:30:31 -0300

    feat: adds github repo to swagger docs
    Signed-off-by: João Prado <joaopedro0128@hotmail.com>

--- configs/swagger.config.ts
+++ configs/swagger.config.ts
@@ -2,7 +2,8 @@ import { DocumentBuilder } from '@nestjs/swagger';
 
 export const swaggerConfig = new DocumentBuilder()
   .setTitle('MKS Movie')
-  .setDescription('The Movie API for MKS')
+  .setDescription('This API was made for a development test requested by MKS.')
   .setVersion('1.0')
+  .setExternalDoc("Github", "https://github.com/jppradoleal/mks-teste")
   .addBearerAuth()
   .build();
--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,9 +1,11 @@
 import { Controller, Get, Redirect } from '@nestjs/common';
+import { ApiExcludeEndpoint } from '@nestjs/swagger';
 
 @Controller()
 export class AppController {
   @Get()
   @Redirect()
+  @ApiExcludeEndpoint()
   redirectToApiDoc() {
     return { url: '/swagger', statusCode: 301 };
   }
